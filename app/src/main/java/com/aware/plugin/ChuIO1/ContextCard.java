88d187ebda38665c955c4c2c1bacf03bb2b4a755
cluo29 <mr.cluo29@gmail.com>
2015-04-24 15:03:31 +0800

    ContextCard
    ContextCard

--- app/src/main/java/com/aware/plugin/ChuIO1/ContextCard.java
+++ app/src/main/java/com/aware/plugin/ChuIO1/ContextCard.java
@@ -1,99 +1,75 @@
 package com.aware.plugin.ChuIO1;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.Handler;
-import android.os.Looper;
+import android.database.Cursor;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
 
 import com.aware.Aware;
-import com.aware.ui.Stream_UI;
+import com.aware.plugin.ChuIO1.Provider.ChuIO1_Data;
 import com.aware.utils.IContextCard;
 
 public class ContextCard implements IContextCard {
+    private static TextView Text_a_0;
+    private static TextView Text_a_1;
+    private static TextView Text_a_2;
+    private static TextView Text_l_0;
+    private static TextView Text_m_0;
+    private static TextView Text_m_1;
+    private static TextView Text_m_2;
+    private static TextView Text_gsmstr_0;
+    private static TextView Text_gsmnstr_0;
+    private static TextView Text_cdmastr_0;
+    private static TextView Text_wifistr_0;
+    private static TextView Text_la_0;
+    private static TextView Text_la_1;
+    private static TextView Text_la_2;
 
-    //Set how often your card needs to refresh if the stream is visible (in milliseconds)
-    private int refresh_interval = 1 * 1000; //1 second = 1000 milliseconds
+    public ContextCard() {};
 
-    //DEMO: we are demo'ing a counter incrementing in real-time
-    private int counter = 0;
-
-    private Handler uiRefresher = new Handler(Looper.getMainLooper());
-    private Runnable uiChanger = new Runnable() {
-        @Override
-        public void run() {
-            counter++;
-
-            //Modify card's content here once it's initialized
-            if( card != null ) {
-                //DEMO display the counter value
-                counter_txt.setText(""+counter);
-            }
-
-            //Reset timer and schedule the next card refresh
-            uiRefresher.postDelayed(uiChanger, refresh_interval);
-        }
-    };
-
-    //Empty constructor used to instantiate this card
-    public ContextCard(){};
-
-    //You may use sContext on uiChanger to do queries to databases, etc.
-    private Context sContext;
-
-    //Declare here all the UI elements you'll be accessing
-    private View card;
-    private TextView counter_txt;
-
-    //Used to load your context card
-    private LayoutInflater sInflater;
-
-    @Override
     public View getContextCard(Context context) {
-        sContext = context;
 
-        //Tell Android that you'll monitor the stream statuses
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(Stream_UI.ACTION_AWARE_STREAM_OPEN);
-        filter.addAction(Stream_UI.ACTION_AWARE_STREAM_CLOSED);
-        context.registerReceiver(streamObs, filter);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View card = inflater.inflate(R.layout.card, null);
+        Text_a_0 = (TextView) card.findViewById(R.id.textView2);
+        Text_a_1 = (TextView) card.findViewById(R.id.textView3);
+        Text_a_2 = (TextView) card.findViewById(R.id.textView4);
+        Text_l_0 = (TextView) card.findViewById(R.id.textView6);
+        Text_m_0 = (TextView) card.findViewById(R.id.textView8);
+        Text_m_1 = (TextView) card.findViewById(R.id.textView9);
+        Text_m_2 = (TextView) card.findViewById(R.id.textView10);
+        Text_gsmstr_0 = (TextView) card.findViewById(R.id.textView12);
+        Text_gsmnstr_0 = (TextView) card.findViewById(R.id.textView14);
+        Text_cdmastr_0 = (TextView) card.findViewById(R.id.textView16);
+        Text_wifistr_0 = (TextView) card.findViewById(R.id.textView18);
+        Text_la_0 = (TextView) card.findViewById(R.id.textView20);
+        Text_la_1 = (TextView) card.findViewById(R.id.textView21);
+        Text_la_2 = (TextView) card.findViewById(R.id.textView22);
 
-        //Load card information to memory
-        sInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        card = sInflater.inflate(R.layout.card, null);
+        Cursor latest = context.getContentResolver().query(ChuIO1_Data.CONTENT_URI, null, null, null, ChuIO1_Data.TIMESTAMP + " DESC LIMIT 1");
+        if (latest != null && latest.moveToFirst()) {
+            Text_a_0.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.A_VALUES_0))) + "m/s^2");
+            Text_a_1.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.A_VALUES_1))) + "m/s^2");
+            Text_a_2.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.A_VALUES_2))) + "m/s^2");
+            Text_l_0.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.L_VALUES_0))) + "lux");
+            Text_m_0.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.M_VALUES_0))) + "μT");
+            Text_m_1.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.M_VALUES_1))) + "μT)");
+            Text_m_2.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.M_VALUES_2))) + "μT");
+            Text_gsmstr_0.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.GSMSTR_VALUES_0))) + "asu");
+            Text_gsmnstr_0.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.GSMNSTR_VALUES_0))) + "asu");
+            Text_cdmastr_0.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.CDMASTR_VALUES_0))) + "dBm");
+            Text_wifistr_0.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.WIFISTR_VALUES_0))) + "dB");
+            Text_la_0.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.LA_VALUES_0))) + "m/s^2");
+            Text_la_1.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.LA_VALUES_1))) + "m/s^2");
+            Text_la_2.setText(String.format("%.1f", latest.getDouble(latest.getColumnIndex(ChuIO1_Data.LA_VALUES_2))) + "m/s^2");
 
-        //Initialize UI elements from the card
-        //DEMO only
-        counter_txt = (TextView) card.findViewById(R.id.counter);
-
-        //Begin refresh cycle
-        uiRefresher.postDelayed(uiChanger, refresh_interval);
-
-        //Return the card to AWARE/apps
-        return card;
-    }
-
-    //This is a BroadcastReceiver that keeps track of stream status. Used to stop the refresh when user leaves the stream and restart again otherwise
-    private StreamObs streamObs = new StreamObs();
-    public class StreamObs extends BroadcastReceiver {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            if( intent.getAction().equals(Stream_UI.ACTION_AWARE_STREAM_OPEN) ) {
-                //start refreshing when user enters the stream
-                uiRefresher.postDelayed(uiChanger, refresh_interval);
-
-                //DEMO only, reset the counter every time the user opens the stream
-                counter = 0;
-            }
-            if( intent.getAction().equals(Stream_UI.ACTION_AWARE_STREAM_CLOSED) ) {
-                //stop refreshing when user leaves the stream
-                uiRefresher.removeCallbacks(uiChanger);
-                uiRefresher.removeCallbacksAndMessages(null);
-            }
+        } else {
+            Text_a_0.setText("no data");
         }
+        if (latest != null && !latest.isClosed()) latest.close();
+
+
+        return card;
     }
 }
