8b0e8d31a9a094f431b9f56370573e4980bb5182
cluo29 <mr.cluo29@gmail.com>
2015-04-24 14:31:21 +0800

    plugin
    1

--- app/src/main/java/com/aware/plugin/ChuIO1/Plugin.java
+++ app/src/main/java/com/aware/plugin/ChuIO1/Plugin.java
@@ -10,25 +10,13 @@ import com.aware.providers.Accelerometer_Provider.Accelerometer_Data;
 import com.aware.providers.Light_Provider.Light_Data;
 import com.aware.providers.Magnetometer_Provider.Magnetometer_Data;
 import com.aware.providers.Telephony_Provider.GSM_Data;
+import com.aware.providers.Telephony_Provider.GSM_Neighbors_Data;
+import com.aware.providers.Telephony_Provider.CDMA_Data;
+import com.aware.providers.WiFi_Provider.WiFi_Data;
+import com.aware.providers.Linear_Accelerometer_Provider.Linear_Accelerometer_Data;
 import com.aware.utils.Aware_Plugin;
 public class Plugin extends Aware_Plugin {
     public static final String ACTION_AWARE_PLUGIN_ChuIO1 = "ACTION_AWARE_PLUGIN_ChuIO1";
-/*
-  ChuIO1_Data.A_VALUES_0 + " real default 0," +
-                    ChuIO1_Data.A_VALUES_1 + " real default 0," +
-                    ChuIO1_Data.A_VALUES_2 + " real default 0," +
-                    ChuIO1_Data.L_VALUES_0 + " real default 0," +
-                    ChuIO1_Data.M_VALUES_0 + " real default 0," +
-                    ChuIO1_Data.M_VALUES_1 + " real default 0," +
-                    ChuIO1_Data.M_VALUES_2 + " real default 0," +
-                    ChuIO1_Data.GSMSTR_VALUES_0 + " real default 0," +
-                    ChuIO1_Data.GSMNSTR_VALUES_0 + " real default 0," +
-                    ChuIO1_Data.CDMASTR_VALUES_0 + " real default 0," +
-                    ChuIO1_Data.WIFISTR_VALUES_0 + " real default 0," +
-                    ChuIO1_Data.LA_VALUES_0 + " real default 0," +
-                    ChuIO1_Data.LA_VALUES_1 + " real default 0," +
-                    ChuIO1_Data.LA_VALUES_2 + " real default 0," +
- */
     public static final String EXTRA_A_VALUES_0 ="A_VALUES_0";
     public static final String EXTRA_A_VALUES_1 ="A_VALUES_1";
     public static final String EXTRA_A_VALUES_2 ="A_VALUES_2";
@@ -94,6 +82,41 @@ public class Plugin extends Aware_Plugin {
                 if( GSM != null && ! GSM.isClosed() ) {
                     GSM.close();
                 }
+
+                Cursor GSMN = getApplicationContext().getContentResolver().query(GSM_Neighbors_Data.CONTENT_URI, null, null, null, GSM_Neighbors_Data.TIMESTAMP + " DESC LIMIT 1");
+                if(GSMN!=null && GSMN.moveToFirst()){
+                    gsmnstr_0 = GSMN.getDouble(GSMN.getColumnIndex(GSM_Neighbors_Data.SIGNAL_STRENGTH));
+                }
+                if( GSMN != null && ! GSMN.isClosed() ) {
+                    GSMN.close();
+                }
+
+
+                Cursor CDMASTR = getApplicationContext().getContentResolver().query(CDMA_Data.CONTENT_URI, null, null, null, CDMA_Data.TIMESTAMP + " DESC LIMIT 1");
+                if(CDMASTR!=null && CDMASTR.moveToFirst()){
+                    cdmastr_0 = CDMASTR.getDouble(CDMASTR.getColumnIndex(CDMA_Data.SIGNAL_STRENGTH));
+                }
+                if( CDMASTR != null && ! CDMASTR.isClosed() ) {
+                    CDMASTR.close();
+                }
+
+                Cursor WIFISTR = getApplicationContext().getContentResolver().query(WiFi_Data.CONTENT_URI, null, null, null, WiFi_Data.TIMESTAMP + " DESC LIMIT 1");
+                if(WIFISTR!=null && WIFISTR.moveToFirst()){
+                    wifistr_0 = WIFISTR.getDouble(WIFISTR.getColumnIndex(WiFi_Data.RSSI));
+                }
+                if( WIFISTR != null && ! WIFISTR.isClosed() ) {
+                    WIFISTR.close();
+                }
+
+                Cursor LA = getApplicationContext().getContentResolver().query(Linear_Accelerometer_Data.CONTENT_URI, null, null, null, Linear_Accelerometer_Data.TIMESTAMP + " DESC LIMIT 1");
+                if(LA!=null && LA.moveToFirst()){
+                    la_0 = LA.getDouble(LA.getColumnIndex(Linear_Accelerometer_Data.VALUES_0));
+                    la_1 = LA.getDouble(LA.getColumnIndex(Linear_Accelerometer_Data.VALUES_1));
+                    la_2 = LA.getDouble(LA.getColumnIndex(Linear_Accelerometer_Data.VALUES_2));
+                }
+                if( LA != null && !LA.isClosed() ) {
+                    LA.close();
+                }
                 ContentValues data = new ContentValues();
                 //require Provider.java here
                 data.put(ChuIO1_Data.TIMESTAMP, System.currentTimeMillis());
@@ -106,6 +129,12 @@ public class Plugin extends Aware_Plugin {
                 data.put(ChuIO1_Data.M_VALUES_1,m_1);
                 data.put(ChuIO1_Data.M_VALUES_2,m_2);
                 data.put(ChuIO1_Data.GSMSTR_VALUES_0,gsmstr_0);
+                data.put(ChuIO1_Data.GSMNSTR_VALUES_0,gsmnstr_0);
+                data.put(ChuIO1_Data.CDMASTR_VALUES_0,cdmastr_0);
+                data.put(ChuIO1_Data.WIFISTR_VALUES_0,wifistr_0);
+                data.put(ChuIO1_Data.LA_VALUES_0,la_0);
+                data.put(ChuIO1_Data.LA_VALUES_1,la_1);
+                data.put(ChuIO1_Data.LA_VALUES_2,la_2);
                 getContentResolver().insert(ChuIO1_Data.CONTENT_URI, data);
                 //Share context
                 context_producer.onContext();
@@ -138,6 +167,17 @@ public class Plugin extends Aware_Plugin {
                 context_motion_meter.putExtra(EXTRA_A_VALUES_0, a_0);
                 context_motion_meter.putExtra(EXTRA_A_VALUES_1, a_1);
                 context_motion_meter.putExtra(EXTRA_A_VALUES_2, a_2);
+                context_motion_meter.putExtra(EXTRA_L_VALUES_0, l_0);
+                context_motion_meter.putExtra(EXTRA_M_VALUES_0, m_0);
+                context_motion_meter.putExtra(EXTRA_M_VALUES_1, m_1);
+                context_motion_meter.putExtra(EXTRA_M_VALUES_2, m_2);
+                context_motion_meter.putExtra(EXTRA_GSMSTR_VALUES_0, gsmstr_0);
+                context_motion_meter.putExtra(EXTRA_GSMNSTR_VALUES_0, gsmnstr_0);
+                context_motion_meter.putExtra(EXTRA_CDMASTR_VALUES_0, cdmastr_0);
+                context_motion_meter.putExtra(EXTRA_WIFISTR_VALUES_0, wifistr_0);
+                context_motion_meter.putExtra(EXTRA_LA_VALUES_0, la_0);
+                context_motion_meter.putExtra(EXTRA_LA_VALUES_1, la_1);
+                context_motion_meter.putExtra(EXTRA_LA_VALUES_2, la_2);
 
                 sendBroadcast(context_motion_meter);
             }
